d816cd8dc0e92a0175f3d78b1281a49bb4b9686f
Laia Mestres <laia.mestres.st@emeal.nttdata.com>
2026-01-20 12:08:49 +0100

    delete respuesta automática del bot

--- src/whatsapp/handlers.js
+++ src/whatsapp/handlers.js
@@ -5,7 +5,7 @@ async function handleIncomingMessage(client, msg, producer, registry, schemaId) 
   if (texto.startsWith('buenas bot')) {
     console.log(`🤖 Invocación detectada: ${msg.body}`);
 
-    await client.sendMessage(msg.from, '✅ Mensaje recibido por el Bot, ¡hola!');
+    //await client.sendMessage(msg.from, '✅ Mensaje recibido por el Bot, ¡hola!');
 
     const message = { id: msg.from, text: msg.body };
     await sendAvroMessage(producer, registry, schemaId, 'whatsapp-in', message);
