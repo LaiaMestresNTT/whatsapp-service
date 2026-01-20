eaad7f610d1d10504ef64539461057ceae71b1d3
Laia Mestres <laia.mestres.st@emeal.nttdata.com>
2026-01-20 11:48:41 +0100

    cambios version api

--- src/whatsapp/client.js
+++ src/whatsapp/client.js
@@ -35,10 +35,10 @@ async function initializeClient() {
       }),*/
 
       // CARGAMOS UNA VERSIÓN MÁS ANTIGUA DE WHATSAPP PARA PODER USAR LAS FUNCIONES ANTIGUAS
-      /*webVersionCache: {
+      webVersionCache: {
           type: 'remote',
           remotePath: 'https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html',
-      },*/
+      },
 
       puppeteer: {
         headless: true,
