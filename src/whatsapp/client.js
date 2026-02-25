85b64733f1bb119f62a4b7b994da18f2545716e7
Laia Mestres <laia.mestres.st@emeal.nttdata.com>
2026-02-25 12:34:50 +0100

    version remote

--- src/whatsapp/client.js
+++ src/whatsapp/client.js
@@ -52,7 +52,12 @@ async function initializeClient() {
         ignoreHTTPSErrors: true,
         authTimeoutMs: 120000,
         qrMaxRetries: 10,
-      }
+      },
+
+      webVersionCache: {
+          type: 'remote',
+          remotePath: 'https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html',
+        },
 
     });
 
